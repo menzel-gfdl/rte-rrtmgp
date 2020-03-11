d5fb51b8c3a47fe45281e8efba20d14e662f331f
Raymond.Menzel <raymond.menzel@noaa.gov>
2020-03-11 14:46:37 -0400

    add some convenience scripts

---
+++ examples/era5/download_input_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ex
+wget ftp://ftp2.gfdl.noaa.gov/perm/Raymond.Menzel/era5-inputs.tar.gz
---
+++ examples/era5/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+year="2010"
+month="March"
+day="01"
+input="era-inputs"
+output="output_data"
+
+mkdir -p ${output}/${year}-${month}-era5
+
+#All-sky.
+time ./era5 ${input}/rrtmgp-data-lw-g256-2018-12-04.nc \
+            ${input}/rrtmgp-data-sw-g224-2018-12-04.nc \
+            ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-level.nc \
+            ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-single.nc \
+            ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-albedo.nc \
+            ${input}/ghg.nc \
+            -H2O -O3 -CH4 ${year} -N2O ${year} -O2 .209e6 -CO2 ${year} \
+            -CFC-11 ${year} -CFC-12 ${year} -CFC-113 ${year} -HCFC-22 ${year} \
+            -o ${output}/${year}-${month}-era5/${year}-${month}-${day}-era5-rte-rrtmgp-allsky.nc \
+            -clouds ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-clouds.nc \
+            -beta ${input}/beta_distribution.nc \
+            -liquid ${input}/clouds/hu_stamnes.nc -ice ${input}/clouds/chou_suarez.nc \
+             -t 1 -T 1 -r-liquid 10. -r-ice 50.
+
+#Clear-sky.
+time ./era5 ${input}/rrtmgp-data-lw-g256-2018-12-04.nc \
+       ${input}/rrtmgp-data-sw-g224-2018-12-04.nc \
+       ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-level.nc \
+       ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-single.nc \
+       ${input}/${year}-${month}-era5/${year}-${month}-${day}-era5-albedo.nc \
+       ${input}/ghg.nc \
+       -H2O -O3 -CH4 ${year} -N2O ${year} -O2 .209e6 -CO2 ${year} \
+       -CFC-11 ${year} -CFC-12 ${year} -CFC-113 ${year} -HCFC-22 ${year} \
+       -o ${output}/${year}-${month}-era5/${year}-${month}-${day}-era5-rte-rrtmgp-clearsky.nc \
+       -t 5 -T 5
